d1ad993c84c110b1cfe50020dedf9a83e5e44f1f
Davidtsiklauri <wiklauri.dato1997@gmail.com>
2020-02-09 19:12:57 +0400

    added socket io

---
+++ node/server.js
@@ -0,0 +1,14 @@
+const app = require('./app');
+const http = require('http').createServer(app);
+var io = require( "socket.io" )( http );
+
+http.listen( 8080, () => console.log( 'port', '8080' ));
+
+
+io.of('message')
+    .on('connection',function(socket){ 
+        console.log('connected'); 
+        socket.on('onMessage', ( data ) => {
+            io.emit( 'recive', data )
+        })
+});
--- react-messanger/src/App.js
+++ react-messanger/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import io from 'socket.io-client';
+
 
 function App() {
+
+  const socket = io('http://localhost:8080/message');
+  socket.on('onMessage', () => {
+     console.log('connection');
+  } );
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <input onKeyUp={handler}/>
   );
+
+  function handler( e ) {
+      if( e.key === 'Enter' ) {
+            socket.emit('onMessage', {
+                data: { data: e.target.value }
+            });
+      }
+  } 
 }
 
 export default App;
